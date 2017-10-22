9497f42796402521a532720899ddea26346477ba
madhava reddy <madhavaster@gmail.com>
2017-10-22 14:44:29 +0530

    Get and Load methods

--- HibernateProject/src/crudexample/TestGetLoad.java
+++ HibernateProject/src/crudexample/TestGetLoad.java
@@ -1,29 +1,22 @@
-package example2;
+package crudexample;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import beans.Student2;
 
-public class Test {
+public class TestGetLoad {
 
 	public static void main(String[] args) {
-		
+
 		Configuration cf = new Configuration();
-		cf.configure("resources/oracle.cfg.xml");
+		cf.configure("resources/oracle2.cfg.xml");
 		SessionFactory sf = cf.buildSessionFactory();
 		Session session = sf.openSession();
-		Transaction t = session.beginTransaction();
-		Student2 st = new Student2();
-		st.setId(102);
-		st.setName("swapna");
-		st.setEmail("swapna@gmail.com");
 		
-		int pk =(int) session.save(st);
-		System.out.println(pk);
-		t.commit();
+		Student2 st = (Student2)session.get(Student2.class, 101);
+		System.out.println("madhava>>>>"+st.getName());
 		session.close();
 		sf.close();
 	}
